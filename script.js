3e90e6513490b863a28e075b9b042c91e9f82c27
aj2000 <hi@ajaysharma.dev>
2023-03-15 21:33:50 +0530

    readme update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+function hasKey(key) {
+  //   write your code here
+}
+
+// Do not change the code below
+const key = prompt("Enter Key.");
+alert(hasKey(key));
